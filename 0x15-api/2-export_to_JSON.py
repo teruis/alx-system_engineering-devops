48f217ede60cb4e564182723ca2f093f8fd83964
teruis <mohamadelassouli2002@gmail.com>
2024-01-29 20:55:43 +0100

    x

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-
-import json
+"""Exports to-do list information for a given employee ID to CSV format."""
+import csv
 import requests
 import sys
+import urllib3
 
+# Disable SSL certificate verification
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def get_user_todo_list():
-    employee_id = int(sys.argv[1])
-    url1 = 'https://jsonplaceholder.typicode.com/users/%s' % employee_id
-    url2 = '%s/todos' % url1
-    todo_list = requests.get(url2).json()
-    user = requests.get(url1).json()
-    completed_todo = []
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id), verify=False).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}, verify=False).json()
 
-    for todo in todo_list:
-        if todo.get('completed') is True:
-            completed_todo.append({"task": todo.get('title'), "completed": True, "username": user.get('username')})
-
-    user_todo_data = {str(employee_id): completed_todo}
-
-    with open(f"{employee_id}.json", "w") as json_file:
-        json.dump(user_todo_data, json_file, indent=4)
-
-    print("Data exported successfully to {}.json".format(employee_id))
-
-
-if __name__ == '__main__':
-    get_user_todo_list()
-
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [
+            writer.writerow([user_id, username, t.get("completed"), t.get("title")])
+            for t in todos
+        ]
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,39 +1,18 @@
 #!/usr/bin/python3
-
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
 
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
 
-def get_all_todos():
-    url_users = 'https://jsonplaceholder.typicode.com/users'
-    users = requests.get(url_users).json()
-
-    all_todos = {}
-
-    for user in users:
-        user_id = user['id']
-        username = user['username']
-        url_todos = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
-        todos = requests.get(url_todos).json()
-
-        user_todos = []
-        for todo in todos:
-            user_todos.append({
-                'username': username,
-                'task': todo['title'],
-                'completed': todo['completed']
-            })
-
-        all_todos[user_id] = user_todos
-
-    return all_todos
-
-
-if __name__ == '__main__':
-    all_todos = get_all_todos()
-
-    with open('todo_all_employees.json', 'w') as json_file:
-        json.dump(all_todos, json_file, indent=4)
-
-    print("Data exported successfully to todo_all_employees.json")
-
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
